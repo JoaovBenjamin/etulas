126cab0c58bbd1db6f742f88aa9d6e10f90502df
Joaovvb <rm98938@fiap.com.br>
2024-05-20 21:38:53 -0300

    Documentation

--- src/main/java/com/example/etulas/controller/paciente/PacienteController.java
+++ src/main/java/com/example/etulas/controller/paciente/PacienteController.java
@@ -30,6 +30,10 @@ import com.example.etulas.model.paciente.Paciente;
 import com.example.etulas.repository.paciente.PacienteRepository;
 import com.example.etulas.service.Paciente.PacienteService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,6 +41,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("paciente")
 @Slf4j
+@Tag(name = "Paciente")
 public class PacienteController {
 
     @Autowired
@@ -45,14 +50,35 @@ public class PacienteController {
     @Autowired
     PacienteRepository repository;
 
+
     @GetMapping
     @ResponseStatus(OK)
+     @Operation(
+        summary = "Listar Paciente",
+        description = "Retorna um array com todos pacientes cadastrados."         
+    )
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Pacientes retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado")
+        }
+    )
     public List<Paciente> buscarPaciente() {
         log.info("Buscando pacientes");
         return service.buscarPaciente();
     }
 
-        @GetMapping("page")
+    @GetMapping("page")
+    @Operation(
+        summary = "Listar paginação de pacientes",
+        description = "Retorna uma paginação de pacientes"
+    )
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Pacientes retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado")
+        }
+    )
     public Page<Paciente> index(
         @RequestParam(required = false) String paciente,
         @RequestParam(required = false) String cpf,
@@ -76,12 +102,32 @@ public class PacienteController {
 
 
     @GetMapping("{id}")
+    @Operation(
+        summary = "Listar Paciente por Id",
+        description = "Retorna um paciente por id"
+    )
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Paciente retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado")
+        }
+    )  
     public ResponseEntity<Paciente> buscarHospitalPorId(@PathVariable Long id) {
         log.info("Buscando paciente com o id {}", id);
         return service.buscarPacientePorId(id);
     }
 
     @GetMapping("cpf/{cpf}")
+    @Operation(
+        summary = "Listar paciente por cpf",
+        description = "Retorna um paciente de acordo com cpf"
+    )
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Paciente retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado")
+        }
+    )  
     public List<Paciente> buscarPacientePorCpf(@PathVariable String cpf) {
         log.info("Buscando paciente com cpf {}", cpf);
         return service.buscarPacientePorCpf(cpf);
@@ -89,6 +135,14 @@ public class PacienteController {
 
     @PostMapping
     @ResponseStatus(CREATED)
+    @Operation(
+        summary = "Criar Paciente",
+        description = "Cria um paciente com os dados do corpo da requisição"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Paciente criado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Validação falhou. Verifique os dados enviados no corpo da requisição")
+    })
     public ResponseEntity<Paciente> criarPaciente(@Valid @RequestBody PacienteDTO dados) {
         log.info("Criando hospital");
         Paciente novoPaciente = service.criarPaciente(dados);
@@ -96,6 +150,14 @@ public class PacienteController {
     }
 
     @PutMapping("{id}")
+    @Operation(
+        summary = "Atualizar Paciente",
+        description = "Atualiza paciente de acordo com os dados no corpo da requisição"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Paciente atualizado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Validação falhou. Verifique os dados enviados no corpo da requisição")
+    })
     public Paciente atualizarPaciente(@PathVariable Long id, PacienteDTO dados) {
         log.info("Atualizando paciente com o id {}", id);
         return service.atualizarPaciente(id, dados);
@@ -103,6 +165,14 @@ public class PacienteController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
+    @Operation(
+        summary = "Deletar Paciente",
+        description = "Deletar paciente com id passado no path"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "Paciente deletado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Not found")
+    })
     public void deletarHospital(@PathVariable Long id) {
         log.info("Deletando paciente com o id", id);
         service.deletarPaciente(id);
